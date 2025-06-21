60038be588079d95af765ed2fb1757ee604b7dfc
nic116seitz <25190512+nic116seitz@users.noreply.github.com>
2025-06-21 07:51:43 -0600

    First 3 components Player Choice, CPU Choice and Round Result completed The three above have been added to the script Determination of round outcome to be added, as well as round counter and win counter

--- Rps.js
+++ Rps.js
@@ -1,1 +1,47 @@
-alert("Test")
+// let rounds = 0; 
+// playerChoice = alert(prompt("Enter a letter of your choice (R = rock S = Scissors P = Paper)"));
+// function randomNum(){
+//  return (Math.random() * 100)
+//}
+let randomNum = 70;// Math.floor(Math.random() * 100);
+let game =""
+console.log("This is the randomNum: " + randomNum)
+
+function cpuChoice() {
+if (randomNum <= 100/3) {
+    return "R";
+  }
+else if (randomNum <= 100 * (2/3) && randomNum >= 100/3) {
+    return "P";
+  }
+else {
+    return "S";
+  }
+}
+
+let playerChoice = prompt("Enter the corresponding letter of your choice R = Rock P = Paper S = Scissors")
+console.log("This is the player Choice: " + playerChoice)
+
+let roundResult = cpuChoice() + playerChoice
+console.log("This is the round result: " + roundResult)
+
+let cpuWin = 0
+let playerWin = 0
+
+function roundWin(roundResult) {
+  if (roundResult === "SS" || "PP" || "RR") {
+    return "Round is a draw";
+  }
+  else if (roundResult === "SP" || "PR" || "RS") {
+     cpuWin += 1  
+     return "CPU wins";
+  } 
+  else 
+    playerWin += 1
+    return "Player wins";
+}
+
+let outcome = roundWin()
+console.log("This is the outcome" + outcome)
+console.log(cpuWin)
+console.log(playerWin)
